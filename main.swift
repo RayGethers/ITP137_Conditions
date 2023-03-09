58aea53667592e7e84a91a83e98eec0e98575ce0
RayGethers <124451441+RayGethers@users.noreply.github.com>
2023-03-09 19:05:36 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,73 @@
+import Foundation
+import Glibc
+
+print("Please enter a string") 
+
+
+ if let input = readLine() {
+    
+
+     if let charValue = mostFrequentCharacter(input){
+  
+      
+      print("The most frequent character is " +  String(charValue))
+
+    
+    } else {
+      print ("error try again")
+  
+
+  
+    } 
+ }
+
+
+
+
+
+
+
+
+func mostFrequentCharacter(_ input: String) -> Character? { 
+  
+  
+
+
+  var maxCount = 0
+  
+  var maxChar: Character?
+  // var len = input.count
+  // print(len)
+
+  for char in input {
+
+    print("This is the char \(char)")
+    var count = 0 
+    
+
+    for otherChar in input {
+        // print("This is the otherChar \(otherChar)")
+        // print("This is the count before line 49 \(count)")
+        if otherChar == char {
+
+      
+        count+=1
+        //print("This is the count before line 51 \(count)")
+        }
+    }
+        if count > maxCount {
+  
+        maxChar = char
+        maxCount = count
+      }
+    
+
+  
+  
+ }
+
+  
+// print(maxCount)
+return maxChar
+  
+} 
